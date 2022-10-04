d8d22591d23ad2e73667df58ade51207a2acff9f
Guilherme Rio-Tinto <guilherme.rio-tinto@kcl.ac.uk>
2022-10-04 22:46:36 +0100

    Make value pair integer instead of string

--- frequencies.py
+++ frequencies.py
@@ -1,9 +1,10 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+import pytest
 
 def frequencies(items):
     no_duplicates = dict.fromkeys(items)
     frequencies = {}
     for i in no_duplicates:
-        frequencies[str(i)] = str(items.count(i))
+        frequencies[str(i)] = items.count(i)
     return frequencies
